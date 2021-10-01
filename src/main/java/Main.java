11081882c1f4f9c0f9eeaef7a8523123a72459da
Валентин <valmak04@gmail.com>
2021-10-01 19:27:14 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        //write your code here
+        int sum = 0, count_negative = 0;
+        Scanner sc = new Scanner(System.in);
+        String input_data = sc.nextLine();
+        String[] data_int = input_data.split(" ");
+        for (String ind_digit : data_int) {
+            int int_digit = Integer.parseInt(ind_digit);
+            if (int_digit < 0){
+                count_negative++;
+                sum += int_digit;
+            }
+        }
+        System.out.println(count_negative + " " + sum);
+
     }
 }
